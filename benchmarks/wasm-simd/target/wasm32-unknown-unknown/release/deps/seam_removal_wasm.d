24eeed3d9b2e553f1160f9549c5f9f6cd7dfabe7
​ <>
2025-06-22 14:29:42 +0900

    Fixing SIMD benchmark

--- benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.d
+++ benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.d
@@ -1,5 +1,6 @@
-/Users/m/git/seams/benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.wasm: src/lib.rs
+/Users/m/git/seams/benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.wasm: src/lib.rs src/simd_helpers.rs
 
-/Users/m/git/seams/benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.d: src/lib.rs
+/Users/m/git/seams/benchmarks/wasm-simd/target/wasm32-unknown-unknown/release/deps/seam_removal_wasm.d: src/lib.rs src/simd_helpers.rs
 
 src/lib.rs:
+src/simd_helpers.rs:
